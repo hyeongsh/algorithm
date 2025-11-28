7d422899b259ef2a4c1780be32aba546ccab4681
hyeongsh <shinhs6384@naver.com>
2025-11-28 17:54:19 +0900

    25-11-28 +1

--- 2025/11_November/day28_29863.java
+++ 2025/11_November/day28_29863.java
@@ -8,7 +8,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
 
-public class Main {
+public class day28_29863 {
     public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
